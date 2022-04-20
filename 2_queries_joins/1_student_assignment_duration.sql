3f86e9871c65d4c87866bdf4d930ffebb418734c
Nika Ptushkina <nika.ptushkina@gmail.com>
2022-04-19 23:44:32 -0700

    BootcampX Queries II

---
+++ 2_queries_joins/1_student_assignment_duration.sql
@@ -0,0 +1,9 @@
+/* 
+Student's Total Assignment Duration;
+Total amount of time that a student has spent on all assignments
+*/
+
+SELECT sum(assignment_submissions.duration) as total_duration
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.name = 'Ibrahim Schimmel';
